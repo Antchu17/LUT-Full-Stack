c2cba488a8c86c46f95144d827720d74b7557818
Antchu17 <hernesniemi.antti@outlook.com>
2021-03-03 01:08:38 +0200

    as

---
+++ README.md
@@ -0,0 +1,6 @@
+# Antti Hernesniemi, Anytime-course: Software Development Skills: Full-Stack 2020-21
+This repository contains my work on the Full-Stack course.
+
+The exercises folder contains the exercises done during the course. The files are organised in folders based on what part of the tasklist the files belong to.
+
+learning-diary-and-video folder contains the learning diary as docx and pdf files and a file containing a Youtube link to a video showing the API.
---
+++ exercises/express/index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const logger = require('./middleware/logger');
+const { timeStamp } = require('console');
+const members = require('./Members');
+
+const app = express();
+
+
+ 
+
+// Init middleware
+//app.use(logger);
+
+//Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Body Parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+//homepage route
+app.get('/', (req, res) => res.render('index', {
+	title: 'Member App',
+	members
+}));
+
+// Set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api/members', require('./routes/api/members'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
